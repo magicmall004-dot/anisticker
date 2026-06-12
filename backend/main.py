2a64274d51307491b85e5b28f3761ce2e656feba
HtunHlaAung <hlaaunghtun68@gmail.com>
2026-06-12 01:43:15 +0000

    fix cors and config

--- backend/main.py
+++ backend/main.py
@@ -45,7 +45,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=ALLOWED_ORIGINS,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
