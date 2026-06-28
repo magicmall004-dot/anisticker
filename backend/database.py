e38bff0a725e6df7b356491b31343433f47abc03
HtunHlaAung <hlaaunghtun68@gmail.com>
2026-06-28 11:53:14 +0000

    Remove sensitive backend files from tracking

--- backend/database.py
+++
@@ -1,9 +0,0 @@
-from supabase import create_client, Client
-from config import get_settings
-from functools import lru_cache
-
-
-@lru_cache
-def get_db() -> Client:
-    s = get_settings()
-    return create_client(s.SUPABASE_URL, s.SUPABASE_SERVICE_KEY)
